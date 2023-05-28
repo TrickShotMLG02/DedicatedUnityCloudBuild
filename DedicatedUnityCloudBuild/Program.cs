6158ae4f6e290d29e7178d807b697c3d89e98974
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2023-05-28 23:34:39 +0200

    Projektdateien hinzufügen.

---
+++ DedicatedUnityCloudBuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DedicatedUnityCloudBuild // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
