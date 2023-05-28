bb29b7e87b392eb024b9e69dfd56d7e780c94e12
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2023-05-29 01:51:06 +0200

    Implemented first config load with logging

--- DedicatedUnityCloudBuild/Program.cs
+++ DedicatedUnityCloudBuild/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using DedicatedUnityCloudBuild.Config;
+using DedicatedUnityCloudBuild.Log;
+using DedicatedUnityCloudBuild.Variables;
 
 namespace DedicatedUnityCloudBuild
 {
     internal class Program
     {
+        private static void initializeInstances()
+        {
+            // Initialize ConfigManager
+            new ConfigManager();
+        }
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Initialize Logger
+            new Logger();
+            Logger.Instance.LogInfoBlock(ProgramVariables.applicationName + " v" + ProgramVariables.applicationVersion, "Thank you for using this Program. If you have any issues, feel free to open an issue on the following page: " + ProgramVariables.repoURL);
+
+            // Initialize all other instances
+            initializeInstances();
         }
     }
 }
